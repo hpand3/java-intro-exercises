7c622b6a2fa48bfa464ff0b9c8e707a3eab3abb2
Harsh Pandya <harsh@AUhpandya.local>
2018-01-11 22:29:17 +1000

    Refactored to shorten the isosceles triangle function

--- src/com/thoughtworks/Diamond.java
+++ src/com/thoughtworks/Diamond.java
@@ -7,33 +7,44 @@ public class Diamond {
         this.symbol = symbol;
     }
 
-    public void isoTriangle(int height) {
-        int maxNumSymbols = 2 * height - 1;
-        int rowSpace = 0;
-        int upperBound, lowerBound = 0;
+    public void drawLine(int maxNumSymbols, int rowSpace) {
 
-        for (int numSymbols = 1; numSymbols <= height; numSymbols += 1) {
+        int upperBound = maxNumSymbols - rowSpace;
+        int lowerBound = rowSpace + 1;
+
+        for (int column = 1; column <= maxNumSymbols; column += 1) {
+
+            if (column > upperBound || column < lowerBound) {
+                System.out.print(' ');
+            } else {
+                System.out.print(this.symbol);
+            }
+
+        }
+        System.out.println();
+    }
+
+    public void topHalfDiamond(int maxNumSymbols, int height) {
+
+        int rowSpace;
+
+        for (int numSymbols = 1; numSymbols <= height-1; numSymbols += 1) {
 
             rowSpace = Math.abs(height - numSymbols);
-            upperBound = maxNumSymbols - rowSpace;
-            lowerBound = rowSpace + 1;
 
-            for (int column = 1; column <= maxNumSymbols; column += 1) {
+            this.drawLine(maxNumSymbols, rowSpace);
 
-                if (column > upperBound || column < lowerBound) {
-                    System.out.print(' ');
-                } else {
-                    System.out.print(this.symbol);
-                }
-
-            }
-            System.out.println();
         }
     }
 
-    public void diamond(int height) {
-        
+    public void isoTriangle(int height) {
+        int maxNumSymbols = 2 * height - 1;
+
+        this.topHalfDiamond(maxNumSymbols, height);
+        this.drawLine(maxNumSymbols, 0);
     }
 
+    public void diamond(int height) {
 
+    }
 }
